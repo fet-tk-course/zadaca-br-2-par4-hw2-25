96fc82667e00d195f723fca242d7b4707706d9da
Amel-Tokic <amel.tokic@fet.ba>
2026-05-08 23:12:11 +0200

    Student A - dodavanje klase

--- models_a.py
+++ models_a.py
@@ -1,5 +1,14 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 
-# TODO: Student A - Definiši svoj SQLModel entitet ovdje
-# 
+class Zivotinja (SQLModel, table=True):
+    ID_zivotinje: Optional[int] = Field(default=None, primary_key=True)
+
+    ime_zivotinje : str = Field (min_length=1, description="Ime zivotinje.")
+    starost: int = Field(ge=0, description="Starost životinje u godinama")
+    tezina: float = Field(gt=0.0, description="Težina životinje u kilogramima")
+    je_dresirana: bool = Field(description="Da li je životinja dresirana")
+    broj_kaveza: int = Field(ge=1, description="Broj kaveza u kojem životinja boravi")
+    opis: Optional[str] = Field(default=None, description="Dodatni opis životinje")
+    zdravstveni_karton: Optional[str] = Field(default=None, description="Bilješke iz zdravstvenog kartona")
+
