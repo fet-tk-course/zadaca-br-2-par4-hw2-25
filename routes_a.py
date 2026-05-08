5071034d2f751d72cab88cbfa18dff67725b29c7
Amel-Tokic <amel.tokic@fet.ba>
2026-05-08 23:49:57 +0200

    Routes_a - Dohvatanje liste svih resursa

--- routes_a.py
+++ routes_a.py
@@ -3,10 +3,9 @@ from sqlmodel import Session, select
 from typing import Optional
 
 from database import get_session
-from models_a import Zivotinja, ZivotinjaCreate, ZivotinjaUpdate
+from models_a import Zivotinja, ZivotinjaCreate, ZivotinjaUpdate 
 
 router = APIRouter(prefix="/resursi_a", tags=["Resurs A"])
-
 
 # Dohvatanje liste svih zivotinja s opcionalnim filterima po vrsti i dresiranosti
 
@@ -16,3 +15,12 @@ def get_all_zivotinje(
     je_dresirana: Optional[bool] = None,
     session: Session = Depends(get_session)
 ):
+    query = select(Zivotinja)
+ 
+    if vrsta_zivotinje is not None:
+        query = query.where(Zivotinja.vrsta_zivotinje == vrsta_zivotinje)
+    if je_dresirana is not None:
+        query = query.where(Zivotinja.je_dresirana == je_dresirana)
+ 
+    zivotinje = session.exec(query).all()
+    return zivotinje
