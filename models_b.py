de879c319d6ca5ca82bea2ffc61ffd5737eb7710
Lejla <lejla.kadusic@fet.ba>
2026-05-09 10:31:54 +0200

    Dodana PerformanceCreate shema za validaciju unosa

--- models_b.py
+++ models_b.py
@@ -12,3 +12,16 @@ class Performance(SQLModel, table=True):
     audience_rating: float 
     max_viewers: int 
     animal_id: int = Field(foreign_key="zivotinja.ID_zivotinje")
+
+    
+
+class PerformanceCreate(SQLModel):
+title: str
+difficulty: int
+description: Optional[str] = None
+required_equipment: bool
+start_time: Optional[str] = None
+audience_rating: float
+max_viewers: int
+animal_id: int
+
