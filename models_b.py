361ca688c994a25712b7737c94fc4731eb630e7e
Lejla <lejla.kadusic@fet.ba>
2026-05-09 10:33:54 +0200

    Dodana PerformanceUpdate shema sa opcionalnim poljima

--- models_b.py
+++ models_b.py
@@ -13,7 +13,7 @@ class Performance(SQLModel, table=True):
     max_viewers: int 
     animal_id: int = Field(foreign_key="zivotinja.ID_zivotinje")
 
-    
+
 
 class PerformanceCreate(SQLModel):
 title: str
@@ -25,3 +25,13 @@ audience_rating: float
 max_viewers: int
 animal_id: int
 
+
+class PerformanceUpdate(SQLModel):
+title: Optional[str] = None
+difficulty: Optional[int] = None
+description: Optional[str] = None
+required_equipment: Optional[bool] = None
+start_time: Optional[str] = None
+audience_rating: Optional[float] = None
+max_viewers: Optional[int] = None
+animal_id: Optional[int] = None
