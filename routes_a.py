cbd028509650227ba38e9772d35d33906fc09c3a
Amel-Tokic <amel.tokic@fet.ba>
2026-05-09 10:32:49 +0200

    GET - lista svih sa filterom po vrsti i dresiranosti

--- routes_a.py
+++ routes_a.py
@@ -6,3 +6,22 @@ from database import get_session
 from models_a import Zivotinja, ZivotinjaCreate, ZivotinjaUpdate 
 
 router = APIRouter(prefix="/resursi_a", tags=["Resurs A"])
+
+
+# Dohvatanje liste svih životinja s opcionalnim filterima po vrsti i dresiranosti
+
+@router.get("/", response_model=list[Zivotinja])
+def get_all_zivotinje(
+    vrsta_zivotinje: Optional[str] = None,
+    je_dresirana: Optional[bool] = None,
+    session: Session = Depends(get_session)
+):
+    query = select(Zivotinja)
+ 
+    if vrsta_zivotinje is not None:
+        query = query.where(Zivotinja.vrsta_zivotinje == vrsta_zivotinje)
+    if je_dresirana is not None:
+        query = query.where(Zivotinja.je_dresirana == je_dresirana)
+ 
+    zivotinje = session.exec(query).all()
+    return zivotinje
