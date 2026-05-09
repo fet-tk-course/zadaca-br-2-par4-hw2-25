41d422e789824248ea3863d7f990b47dc39af094
Lejla <lejla.kadusic@fet.ba>
2026-05-09 11:12:06 +0200

    Implementiran POST endpoint i uspjesno testiran u Swaggeru

--- routes_b.py
+++ routes_b.py
@@ -1,6 +1,19 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
+from typing import List, Optional
 
 from database import get_session
+from models_b import Performance, PerformanceCreate, PerformanceUpdate 
 
-router = APIRouter(prefix="/resursi_b", tags=["Resurs B"])
+router = APIRouter(prefix="/performances", tags=["Performances"])
+
+
+@router.post("/", status_code=201)
+def create_performance(performance_data: PerformanceCreate, session: Session = Depends(get_session)):
+
+    new_performance = Performance.from_orm(performance_data)
+    session.add(new_performance)
+    session.commit()
+    session.refresh(new_performance)
+    return new_performance
+
